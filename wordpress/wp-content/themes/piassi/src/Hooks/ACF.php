7aab45ec224f7d2425848589acf1bef62da9da74
Thales Piassi <thales.piassi.nunes@gmail.com>
2020-08-06 22:00:54 -0300

    Add bundle hooks

--- wordpress/wp-content/themes/piassi/src/Hooks/ACF.php
+++ wordpress/wp-content/themes/piassi/src/Hooks/ACF.php
@@ -15,6 +15,8 @@ class ACF extends Hook {
 	 */
 	public function __construct() {
         $this->add_action( 'acf/format_value/type=image', 'image_format_value', 20, 3 );
+		$this->add_filter( 'acf/settings/show_admin', 'acf_settings_show_admin' );
+		$this->add_filter( 'acf/settings/url', 'acf_settings_url' );
 	}
 
 	/**
@@ -27,5 +29,25 @@ class ACF extends Hook {
 	 */
 	public function image_format_value( $value, $post_id, $field ) {
 		return is_array( $value ) ? Image::from_acf( $value ) : $value;
+	}
+
+	/**
+	 * Customize the url setting to fix incorrect asset URLs.
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	public function acf_settings_url( $url ): string {
+		return MY_ACF_URL;
+	}
+
+	/**
+	 * Hide the ACF admin menu item.
+	 *
+	 * @param bool $show_admin
+	 * @return bool
+	 */
+	public function acf_settings_show_admin( $show_admin ): bool {
+		return false;
 	}
 }
