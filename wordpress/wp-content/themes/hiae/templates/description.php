aec8b54ce5e3dd870f247f3a9ae4b5aa50abe857
Eder Silva <contato@edersilva.com>
2020-08-08 16:32:43 -0300

    Css section description

--- wordpress/wp-content/themes/hiae/templates/description.php
+++ wordpress/wp-content/themes/hiae/templates/description.php
@@ -6,9 +6,9 @@
 
 			<div class="welcome">
 				<div class="section-title">
-					<h3>Eder Silva</h3>
-					<h2>Olá, é um prazer tê-lo por aqui.</h2>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+					<h3 class="detail">Eder Silva</h3>
+					<h2 class="title line line-bottom">Olá, é um prazer tê-lo por aqui.</h2>
+					<p class="subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
 				</div>
 			</div>
 
@@ -17,25 +17,25 @@
 				<div class="skill">
 					<h4>HTML5 & CSS3</h4>
 					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered</p>
-					<span class="fab fa-html5"></span>
+					<span class="icon fab fa-html5"></span>
 				</div>
 
 				<div class="skill">
 					<h4>Performance</h4>
 					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered</p>
-					<span class="fas fa-bolt"></span>
+					<span class="icon fas fa-bolt"></span>
 				</div>
 
 				<div class="skill">
 					<h4>Responsividade</h4>
 					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered</p>
-					<span class="fas fa-tablet-alt"></span>
+					<span class="icon fas fa-tablet-alt"></span>
 				</div>
 
 				<div class="skill">
 					<h4>Produtividade</h4>
 					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered</p>
-					<span class="fas fa-rocket"></span>
+					<span class="icon fas fa-rocket"></span>
 				</div>
 
 			</div>
