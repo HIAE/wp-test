f84edf091ea9a3f4f8104dfe64cdfb71bd330d02
Thales Piassi <thales.piassi.nunes@gmail.com>
2020-08-08 21:37:25 -0300

    fix address variable name

--- wordpress/wp-content/themes/piassi/components/social-networks/template.php
+++ wordpress/wp-content/themes/piassi/components/social-networks/template.php
@@ -9,13 +9,13 @@
 
 ?>
 <?php if ( $social_networks ) : ?>
-<ul class="_social-networks m-0 list-unstyled d-flex <?php echo $class; ?>">
+<ul class="_social-networks m-0 list-unstyled <?php echo $class; ?>">
     <?php foreach ( $social_networks as $social_network ) : ?>
     <li class="ml-3">
         <a
             class="flex-center"
             title="<?php echo $social_network['name']; ?>"
-            href="<?php echo $social_networks['address']; ?>"
+            href="<?php echo $social_network['address']; ?>"
             target="_blank"
             rel="noopener noreferrer">
             <i class="icon icon-<?php echo $social_network['icon']; ?>"></i>
