e545897f56da09637c534ea43997cae06a4eb2eb
Eder Silva <contato@edersilva.com>
2020-08-09 00:54:49 -0300

    Css section services

--- wordpress/wp-content/themes/hiae/templates/services.php
+++ wordpress/wp-content/themes/hiae/templates/services.php
@@ -6,26 +6,30 @@
 
 			<article>
 				
-				<div class="section-title">
-					<h3>Serviços</h3>
-					<h2>Estes são os meus serviços</h2>
-				</div>
+				<div class="post">
 
-				<div class="entry">
+					<div class="section-title">
+						<h3 class="detail">Serviços</h3>
+						<h2 class="title line line-bottom">Estes são os meus serviços</h2>
+					</div>
 
-					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-					</p>
+					<div class="entry">
 
-					<p>
-						It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</p>
+						<p class="featured">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+						</p>
 
-					<p>
-						Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-					</p>
+						<p>
+							It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+						</p>
 
-					<a class="button" href="#" title="Entrar em contato">Entrar em contato</a>
+						<p>
+							Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
+						</p>
+
+						<a class="buttons" href="#" title="Entrar em contato">Entrar em contato</a>
+
+					</div>
 
 				</div>
 
@@ -33,7 +37,7 @@
 
 			<aside>
 				<blockquote>
-					<p>There are many variations of passages of Lorem Ipsum available</p>
+					<p>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."</p>
 					<footer>Tony Stark, Stark industries</footer>
 				</blockquote>
 			</aside>
